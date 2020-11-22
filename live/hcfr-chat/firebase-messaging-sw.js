66bcf13d26c39132e7108d36ce8238bd0db6c8ea
Chris Johnson <binbash0597@gmail.com>
2020-11-21 21:57:22 -0500

    files

--- live/hcfr-chat/firebase-messaging-sw.js
+++
@@ -1,5 +0,0 @@
-importScripts('https://www.gstatic.com/firebasejs/8.0.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.0.0/firebase-messaging.js');
-importScripts('../scripts/init-firebase.js');
-
-firebase.messaging();
